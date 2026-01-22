e69ca82af14ea8b176c6cdda13dd00662c3c93f4
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-01-22 22:47:19 +1300

    Edited server package.json file

--- FUTURE_FS_01/Backend/server.js
+++ FUTURE_FS_01/Backend/server.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5000;
 const app = express()
-const contactRouter = require('../Backend/routes/contactRoutes')
+const contactRoute = require('../Backend/routes/contactRoutes')
+const analyticsRoute = require('./routes/analyticsRoute')
 require('dotenv').config()
 app.use(cors({
     origin:process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials:true
 }))
 app.use(express.json())
+app.use(cookieParser())
 mongoose.connect(process.env.DATABASE_URL)
 .then(()=>console.log('MongonDB successfully connected'))
 .catch(err=>console.error(err))
-app.use('/api/contact',contactRouter)
+app.use('/api/contact',contactRoute)
+app.use('api/analytics',analyticsRoute)
 app.get('/api/health', (req, res) => {
     res.json({
         status: 'OK',
